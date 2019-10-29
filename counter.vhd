44e6ea68ef8e6e9b36e1fbaf3af81d2f673df1b3
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2019-10-28 20:35:46 -0400

    made counter count up instead of down and had it compare to different numbers based on latched_cbs

--- counter.vhd
+++ counter.vhd
@@ -7,6 +7,7 @@ entity counter is
         clk:        in std_logic;
         en:         in std_logic;
         cbs:        in std_logic;
+        latched_cbs:    in std_logic;
         set:        in std_logic;
 		reset:			in std_logic;
         is_zero:    out std_logic;
@@ -20,27 +21,35 @@ architecture counter_arch of counter is
 	 
 begin
 
-    process(clk, en, cbs, set, count, reset)
+    process(clk, en, cbs, set, count, reset, latched_cbs)
     begin
 		  if(reset = '1') then
 			   count <= to_unsigned(0, 10);
         elsif (clk'event and clk='1') then -- rising edge of clock
             if (set='1') then -- preference to set
                 if (cbs='1') then
-                    count <= to_unsigned(767, 10); -- 6144
+                    count <= to_unsigned(0, 10); -- 6144
                 else
-                    count <= to_unsigned(131, 10); -- 1056
+                    count <= to_unsigned(0, 10); -- 1056
                 end if;
             elsif (en='1') then -- if enabled and not zero
-                count <= count - 1; -- decrement the count on clock
+                count <= count + 1; -- increment the count on clock
             end if;
         end if;
 
         -- assert is zero when the time's right
-        if (count=0) then
-            is_zero <= '1';
-			else
-			is_zero <= '0';
+        if (latched_cbs='0') then
+            if (count=131) then
+                is_zero <= '1';
+            else
+                is_zero <= '0';
+            end if;
+        else
+            if (count=767) then
+                is_zero <= '1';
+            else
+                is_zero <= '0';
+            end if;
         end if;
         -- put count up on the value bus all the time
         count_val <= std_logic_vector(count); -- removed downto here
